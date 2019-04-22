c74fbedf65de5f4c323fc4d0e6a42128a907c8c1
Wesley Chiang <westhechiang@gmail.com>
2019-04-22 01:29:47 -0700

    style(Main): Adds a backgroundImage for mobile view for Main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,9 +2,17 @@ import * as React from 'react';
 import { Flex } from '@westhechiang/flex';
 import { WorkExperience } from './components/WorkExperience';
 import { Education } from './components/Education';
+import MobileBackgroundImage from './assets/mobile_pattern.png';
 
 export const Main = () => (
-  <Flex width={1} height="100%" flexDirection="column">
+  <Flex
+    width={1}
+    height="100%"
+    flexDirection="column"
+    backgroundImage={[`url(${MobileBackgroundImage})`, 'none']}
+    backgroundSize={100}
+    backgroundRepeat="repeat"
+  >
     <WorkExperience />
     <Education />
   </Flex>
