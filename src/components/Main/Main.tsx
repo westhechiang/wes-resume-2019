a2e91d657b380a5d19ab256885fe8434ac97d7b3
Wesley Chiang <westhechiang@gmail.com>
2019-05-10 03:45:30 -0700

    Update Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Flex } from '@westhechiang/flex';
 import { WorkExperience } from './components/WorkExperience';
 import { Education } from './components/Education';
-import MobileBackgroundImage from './assets/mobile_pattern.png';
 
 export const Main = () => (
   <Flex
